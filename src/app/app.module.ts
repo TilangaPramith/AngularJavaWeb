0b51c50e98c5769c64ae4e002d7acde2e116516d
pramithvidusara@gmail.com <43032202+TilangaPramith@users.noreply.github.com>
2020-08-31 02:16:45 +0530

    add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IntroductionComponent } from './introduction/introduction.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { ContentComponent } from './content/content.component';
+import { TestimonialComponent } from './testimonial/testimonial.component';
+import { ClientComponent } from './client/client.component';
+import { PricingComponent } from './pricing/pricing.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IntroductionComponent,
+    GalleryComponent,
+    ContentComponent,
+    TestimonialComponent,
+    ClientComponent,
+    PricingComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
